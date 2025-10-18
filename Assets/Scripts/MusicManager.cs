33c704001f04efa34d665b0ca6571f58e519a050
qsjddejesus <qsjddejesus@tip.edu.ph>
2025-10-19 00:05:47 +0800

    just changed the script for the audio
    not sure if working pa

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,22 +2,9 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    private static MusicManager instance;
-    
     void Awake()
     {
-        Debug.Log("MusicManager Awake called!");
-        
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-            Debug.Log("Music set to DontDestroyOnLoad!");
-        }
-        else if (instance != this)
-        {
-            Debug.Log("Duplicate music manager found, destroying...");
-            Destroy(gameObject);
-        }
+        DontDestroyOnLoad(this.gameObject);
+        Debug.Log("=== MUSIC MANAGER: DontDestroyOnLoad called! ===");
     }
 }
