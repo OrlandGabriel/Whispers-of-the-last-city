91ff84748c2e07d220e814c350f1648ab0d37b40
qsjddejesus <qsjddejesus@tip.edu.ph>
2025-11-03 22:25:32 +0800

    level 2-3

--- Assets/Scripts/LevelLoader 2-3.cs
+++ Assets/Scripts/LevelLoader 2-3.cs
@@ -1,26 +1,27 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader2_3 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("TRIGGER DETECTED! Collided with: " + other.name + " | Tag: " + other.tag);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
-    private void OnTriggerEnter (Collider other)
-    {
-        if (other.tag == "LevelExit")    
+        // Check if it's the Player entering the exit
+        if (other.CompareTag("Player") || other.name == "Player")
         {
+            Debug.Log("✅ Player detected! Loading Level 3...");
+            
+            // Unlock Level 3
+            PlayerPrefs.SetInt("Level3Unlocked", 1);
+            PlayerPrefs.Save();
+            Debug.Log("Level 3 unlocked and saved!");
+            
             SceneManager.LoadScene("Level 3");
         }
-    }    
-}   
+        else
+        {
+            Debug.Log("❌ Not the player. Got: " + other.name);
+        }
+    }
+}
