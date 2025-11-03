efb63d5cc8258b03ebca646f208570d66a3130f1
kentodev19 <seanaudreyferrer34@gmail.com>
2025-11-04 00:35:18 +0800

    Level 1-2

--- Assets/Scripts/LevelLoader 1-2.cs
+++ Assets/Scripts/LevelLoader 1-2.cs
@@ -1,26 +1,37 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader1_2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Next Level Settings")]
+    public string nextLevelName = "Level 2";
+    public string levelKey = "UnlockedLevel"; // Key for saving progress
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-
-    }
-    
-    private void OnTriggerEnter (Collider other)
-    {
-        if (other.tag == "LevelExit")    
+        if (other.CompareTag("Player")) // Make sure your player has the "Player" tag
         {
-            SceneManager.LoadScene("Level 2");
+            UnlockNextLevel();
+            LoadNextLevel();
         }
-    }    
-}   
+    }
+
+    void UnlockNextLevel()
+    {
+        // Get current progress
+        int unlockedLevel = PlayerPrefs.GetInt(levelKey, 1); // Default is level 1
+
+        // If level 2 is higher than current progress, unlock it
+        if (unlockedLevel < 2)
+        {
+            PlayerPrefs.SetInt(levelKey, 2); // Save that Level 2 is now unlocked
+            PlayerPrefs.Save();
+            Debug.Log("✅ Level 2 unlocked and progress saved!");
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
